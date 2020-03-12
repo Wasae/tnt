60cf4442947ffd32e9d7f8352f80b5163d881ca5
Abdul Wasae Momin <mominwasae8@gmail.com>
2020-03-12 19:30:57 +0530

    package genertor change for edit

--- frontendwork/scripts/helpers/addpackagehelper.js
+++ frontendwork/scripts/helpers/addpackagehelper.js
@@ -1,47 +1,77 @@
 const packageGenerator=(
     function(){
         function PackageHtml(params) {
-            return packageFormer()
+            return packageFormer(params)
         }
 
-        function packageFormer(params) {
-            let pkgid=create_UUID()
+        function packageFormer(params) {            
+            let pkgid=params && params.PackageId?params.PackageId: create_UUID()
             let packageHTML="<fieldset id='"+pkgid+"'>"        
             packageHTML+="<table><tbody>"
             packageHTML+="<tr><td>Display Image : </td><td>"+getFileUploadControl(pkgid)+"</td></tr>"
-            packageHTML+="<tr><td>Title : </td><td>"+getPackageTitleControls(pkgid)+"</td></tr>"
-            packageHTML+="<tr><td>Price : </td><td>"+getPackagePriceControls(pkgid)+"</td></tr>"
-            packageHTML+="<tr><td>Inclusions : </td><td><fieldset><div id='pkginclusion_"+pkgid+"'>"+getPackageInclusionControls()+"</div><button class='addinclusion'>+</button></fieldset></td></tr>"
-            packageHTML+="<tr><td>Accomodations : </td><td><fieldset><div id='pkgaccomodations_"+pkgid+"'>"+getAccomodationControls()+"</div><button class='addaccomodation'>+</button></fieldset></td></tr>"
-            packageHTML+="<tr><td>Daywise Description : </td><td><fieldset><div id='pkgdaywisedescription_"+pkgid+"'>"+getPackageDayWiseDescriptionControls()+"</div><button class='addday'>+</button></fieldset></td></tr>"
+            packageHTML+="<tr><td>Title : </td><td>"+getPackageTitleControls(pkgid,params.title)+"</td></tr>"
+            packageHTML+="<tr><td>Price : </td><td>"+getPackagePriceControls(pkgid,params.price)+"</td></tr>"
+            packageHTML+="<tr><td>Inclusions : </td><td><fieldset><div id='pkginclusion_"+pkgid+"'>"+getPackageInclusionControls(params.inclusion)+"</div><button class='addinclusion'>+</button></fieldset></td></tr>"
+            packageHTML+="<tr><td>Accomodations : </td><td><fieldset><div id='pkgaccomodations_"+pkgid+"'>"+getAccomodationControls(params.accomodations)+"</div><button class='addaccomodation'>+</button></fieldset></td></tr>"
+            packageHTML+="<tr><td>Daywise Description : </td><td><fieldset><div id='pkgdaywisedescription_"+pkgid+"'>"+getPackageDayWiseDescriptionControls(params.daydescription)+"</div><button class='addday'>+</button></fieldset></td></tr>"
             packageHTML+="</tbody></table>"
             //packageHTML+="<button class='removePackage'>Remove</button>"
             packageHTML+="</fieldset>"
             return packageHTML
         }
 
-        function getFileUploadControl(params) {
-            return "<input type='file' id='pkgfileupload_"+params+"'/>"
+        function getFileUploadControl(params,val) {
+            if (val) {
+                return "<input type='file' id='pkgfileupload_"+params+"' value='"+val+"'/>"
+            } 
+            return "<input type='file' id='pkgfileupload_"+params+"'/>"                       
         }
 
-        function getPackageTitleControls(params) {
+        function getPackageTitleControls(params,val) {
+            if (val) {
+                return "<input type='text' data-package-title='packageTitle' id='pkgtitle_"+params+"' value='"+val+"'/>"
+            }
             return "<input type='text' data-package-title='packageTitle' id='pkgtitle_"+params+"'/>"
         }
 
-        function getPackagePriceControls(params) {
+        function getPackagePriceControls(params,val) {
+            if (val) {
+                return "<input type='text' data-package-price='packagePrice' id='pkgprice_"+params+"' value='"+val+"'/>"
+            } 
             return "<input type='text' data-package-price='packagePrice' id='pkgprice_"+params+"'/>"
         }
 
-        function getPackageInclusionControls(params) {
+        function getPackageInclusionControls(params,val) {
+            if (val && val.length!=0) {
+                let html=""
+                html+=val.map(function(d) {
+                            return "<div><input type='text' data-package-inclusions='packageInclusions' value='"+d+"'/><button class='removeinc'>-</button><br></div>"
+                      }).join('')
+                return html
+            }
             return "<div><input type='text' data-package-inclusions='packageInclusions'/><button class='removeinc'>-</button><br></div>"
         }
 
-        function getAccomodationControls(params) {
+        function getAccomodationControls(params,val) {
+            if (val && val.length!=0) {
+                let html=""
+                html+=val.map(function(d) {
+                            return "<div><input type='text' data-package-accomodations='packageAccomodations' value='"+d+"'/><button class='removeinc'>-</button><br></div>"
+                      }).join('')
+                return html
+            }
             return "<div><input type='text' data-package-accomodations='packageAccomodations'/><button class='removeinc'>-</button><br></div>"
         }
 
-        function getPackageDayWiseDescriptionControls(params) {
-            return "<div><textarea placeholder='Day Description'></textarea><button class='removeinc'>-</button></div>"            
+        function getPackageDayWiseDescriptionControls(params,val) {
+            if (val && val.length!=0) {
+                let html=""
+                html+=val.map(function(d) {
+                            return "<div><textarea placeholder='Day Description' value='"+d+"'></textarea><button class='removeinc'>-</button></div>"
+                      }).join('')
+                return html
+            }
+            return "<div><textarea placeholder='Day Description'></textarea><button class='removeinc'>-</button></div>"
         }
 
         return{
